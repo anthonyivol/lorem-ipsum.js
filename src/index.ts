4dc5165b862d5f1b06c3cce776225adb169c1ac6
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-02-10 00:36:56 -0500

    Start main script

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
-// wait for it...
-
+/*
 export class LoremIpsum() {
   // This class is an improved interface that lets you 
   // create generator instances so you don't have to 
   // keep passing options.
 }
+*/
 
-const loremIpsum = () => {
-  // This function is 100% compatible with v1
+import Generator, { Prng } from './lib/generator'
+
+export interface LoremIpsumParams {
+  count?: number
+  format?: string
+  paragraphLowerBound?: number
+  paragraphUpperBound?: number
+  random?: Prng
+  sentenceLowerBound?: number
+  sentenceUpperBound?: number
+  units?: string
+  words?: string[]
 }
+
+const loremIpsum = ({
+  count = 1,
+  format = 'plain',
+  paragraphLowerBound = 3,
+  paragraphUpperBound = 7,
+  random,
+  sentenceLowerBound = 5,
+  sentenceUpperBound = 15,
+  units = 'sentences',
+  words,
+}: LoremIpsumParams): string => {
+  const generator = new Generator({
+    paragraphs: {
+      min: paragraphLowerBound,
+      max: paragraphUpperBound,
+    },
+    sentences: {
+      min: sentenceLowerBound,
+      max: sentenceUpperBound,
+    },
+    words,
+  })
+
+  return ''
+}
+
 export default loremIpsum
